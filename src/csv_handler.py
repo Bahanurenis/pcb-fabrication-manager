a88de16516aa07851ea693caca45e1eaa5ea9ee2
Bahanurenis <bahanurenis@gmail.com>
2023-12-18 21:47:38 +0100

    feat: add change row value

--- src/csv_handler.py
+++ src/csv_handler.py
@@ -1,11 +1,13 @@
 from pathlib import Path
 import pandas as pd
 from pfm_file import PfmCsv, FileTypeEnum
+from config import Config
 
 
 class PfmCsvHandler:
-    def __init__(self, csv_file: PfmCsv):
+    def __init__(self, csv_file: PfmCsv, config: Config):
         self._df: pd.DataFrame
+        self._config = config
         if csv_file.file_type != FileTypeEnum.CSV:
             raise TypeError(
                 f" file extension should be '.csv', given file extension was {input_csv.file_type.value}"
@@ -13,12 +15,26 @@ class PfmCsvHandler:
         with open(csv_file.file_path, "r") as file:
             self._df = pd.read_csv(file, on_bad_lines="warn")
 
-    def get_columns(self):
-        _columns: pd.Index = self._df.columns
-        return _columns.values
+    #  print(self._df)
 
-    def update_csv_data(self, new_columns: dict) -> None:
+    def _get_columns(self):
+        return self._df.columns.values
+
+    def _update_columns(self):
+        _csv_columns: pd.Index.values = self._get_columns()
+        _new_columns: dict = {}
+        for header in self._config.headers:
+            if header.name not in _csv_columns and header.required == True:
+                raise Exception(
+                    f"{header.name} is required ,but it couldn't find in the csv columns"
+                )
+                break
+            else:
+                _new_columns[header.name] = header.mapping_name
         self._df.rename(columns=new_columns, inplace=True)
+
+    def _replace_row_values(self):
+        pass
 
     def save_csv_data(self, output_csv_path: Path):
         self._df.to_csv(output_csv_path, encoding="utf-8", index=False)
